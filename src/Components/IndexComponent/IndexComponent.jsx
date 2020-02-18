b8541058a90c1f4e8e818f89bba3f382f36415cc
Tony <tkrikoriam@achs.cl>
2020-02-18 09:58:04 -0300

    Crear componente AddItem Button

--- src/Components/IndexComponent/IndexComponent.jsx
+++ src/Components/IndexComponent/IndexComponent.jsx
@@ -1,19 +1,28 @@
 import React, { Component } from "react";
 import TextField from "../TextField/TextField";
+import AddItemButton from "../AddItemButton/AddItemButton";
 
 class IndexComponent extends Component {
   state = {};
   render() {
     const textFields = {
       smallText: "Debe escribir el item que desea ingresar",
-      label: "Ingresar Items"
+      label: "Ingresar Items",
+      text: "Grabar Item"
     };
     return (
-      <div className="row">
-        <div className="col-md-6">
-          <TextField {...textFields} />
+      <>
+        <div className="row">
+          <div className="col-md-6">
+            <TextField {...textFields} />
+          </div>
         </div>
-      </div>
+        <div className="row">
+          <div className="col">
+            <AddItemButton {...textFields} />
+          </div>
+        </div>
+      </>
     );
   }
 }
